c0ca09af71cb62580b38349c34ea5d024039f6ab
Cory Fitz <coryalanfitz@gmail.com>
2022-01-09 00:47:14 +0000

    Git ignore

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jw+-xs5v*423z)tdh%y(81my7=-q(dzxt*^&_g#lb^6t=igbvl'
+#SECRET_KEY = 'django-insecure-jw+-xs5v*423z)tdh%y(81my7=-q(dzxt*^&_g#lb^6t=igbvl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
